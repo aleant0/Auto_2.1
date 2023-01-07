bd9830265f263be15b6e209d279dfce3b516c633
Alex <alalant50@gmail.com>
2023-01-07 17:12:42 +0100

    Validation Test added

--- src/test/java/ru/netology/CardApplicationTest.java
+++ src/test/java/ru/netology/CardApplicationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebDriver;
@@ -47,5 +48,27 @@ public class CardApplicationTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual.trim());
     }
 
+    @Test
+    void nameValidationTest() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("input[name=\"name\"]")).sendKeys("Alex");
+        driver.findElement(By.cssSelector("input[name=\"phone\"]")).sendKeys("+79999999999");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        WebElement nameForm = driver.findElement(By.cssSelector("span[data-test-id=\"name\"]"));
+        String actual = nameForm.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", actual.trim());
+    }
 
+    @Test
+    void phoneValidationTest() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("input[name=\"name\"]")).sendKeys("Алекс");
+        driver.findElement(By.cssSelector("input[name=\"phone\"]")).sendKeys("+799999999999");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        WebElement phoneForm = driver.findElement(By.cssSelector("span[data-test-id=\"phone\"]"));
+        String actual = phoneForm.findElement(By.className("input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", actual.trim());
+    }
 }
